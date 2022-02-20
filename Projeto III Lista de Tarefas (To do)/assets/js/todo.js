35ef2a95dcd701d0c81316988299c889bda02c16
Jefferson Eduardo <jeff.rib10e@gmail.com>
2022-02-20 16:22:49 -0300

    Seção: Projeto lista de tarefas (To do)
    Conclusão do projeto lista de tarefas

--- Projeto III Lista de Tarefas (To do)/assets/js/todo.js
+++ Projeto III Lista de Tarefas (To do)/assets/js/todo.js
@@ -1,28 +1,58 @@
-let data = [{
-    id: 1,
-    title: 'Estudar HTML (HCODE)'
-}, {
-    id: 2,
-    title: 'Estudar CSS (HCODE)'
-}, {
-    id: 3,
-    title: 'Estudar JavaScript (Luiz Otávio)'
-}];
+let data = [];
 
-data.forEach(task => {
-    let li = document.createElement('li');
+function renderToDo(){
+    document.querySelector('#to-do').innerHTML = ``;
+    data.forEach(task => {
+        let li = document.createElement('li');
 
-    li.innerHTML = `
-        <input type="checkbox" id="task-${task.id}">
-        <label for="task-${task.id}">${task.title}</label></li>
-    `;
+        li.innerHTML = `
+            <input type="checkbox" id="task-${task.id}">
+            <label for="task-${task.id}">${task.title}</label></li>
+            <button type="button">x</button>
+        `;
 
-    li.querySelector('input').addEventListener("change", e => {
-        if(e.target.checked){
-            li.classList.add('complete');
-        }else{
-            li.classList.remove('complete');
-        }
+        li.querySelector('input').addEventListener("change", e => {
+            if(e.target.checked){
+                li.classList.add('complete');
+            }else{
+                li.classList.remove('complete');
+            }
+        });
+
+        li.querySelector('button').addEventListener('click', e =>{
+            let button = e.target;
+            let li = button.parentNode;
+            let input = li.querySelector('input');
+            let id = input.id;
+            let idArray = id.split('-');
+            let todoID = idArray[1];
+            let title = li.querySelector('label').innerText;
+
+            if(confirm(`Deseja realmente excluir a tarefa ${title}?`)){
+                data = data.filter(task => (task.id !== parseInt(todoID)));
+                renderToDo();
+            }
+        });
+
+        document.querySelector('#to-do').append(li);
     });
-    document.querySelector('#to-do').append(li);
+}
+
+// Pegando o valor digitado pelo usuário
+let newTask = document.querySelector('#new-task').addEventListener('keyup', e => {
+    /*  Verificação se a tela Enter com clicada, caso tenha sido, ele irá pegar 
+     *  o valor do target (campo).
+     */ 
+    if(e.key === 'Enter'){
+        data.push({
+            id: data.length+1,
+            title: e.target.value
+        });
+
+        e.target.value = ``;
+
+        renderToDo();
+    }
 });
+
+renderToDo();
